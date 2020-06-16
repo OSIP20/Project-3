ea2e2c2aca5ea32f018616921e9ffc26887ea721
sahildandekar <66352515+sahildandekar@users.noreply.github.com>
2020-06-16 20:01:02 +0530

    Add files via upload

---
+++ basic_mtcnn.py
@@ -0,0 +1,30 @@
+import cv2
+from mtcnn.mtcnn import MTCNN
+detector = MTCNN()
+
+cap = cv2.VideoCapture(0)
+while True: 
+    #Capture frame-by-frame
+    __, frame = cap.read()
+    
+    #Use MTCNN to detect faces
+    result = detector.detect_faces(frame)
+    if result != []:
+        for person in result:
+            bounding_box = person['box']
+            
+    
+            cv2.rectangle(frame,
+                          (bounding_box[0], bounding_box[1]),
+                          (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
+                          (0,155,255),
+                          2)
+    
+            
+    #display resulting frame
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) &0xFF == ord('q'):
+        break
+#When everything's done, release capture
+cap.release()
+cv2.destroyAllWindows()
