04afdf4031e63329f1f1f9cc4d4bdd0e1dcb74c1
Sarang Chougs <sarangchouguley284@gmail.com>
2020-07-03 21:26:10 +0530

    Added face detection module

---
+++ face_detection/main.py
@@ -0,0 +1,21 @@
+'''
+Description: Main file to run the face detection using MTCNN algorithm
+Author: Aayushi Jaiswal
+Team no: 3
+Project no: 3
+Command to run this file: ' python main.py -i "input image" '
+'''
+
+from classes.facedetector import FaceDetection
+import argparse
+
+# parsing arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--input_image", type = str, help = "path to input image")
+args = vars(ap.parse_args())
+
+
+if __name__ == "__main__":
+
+    fd = FaceDetection(img = args["input_image"])
+    fd.run()
